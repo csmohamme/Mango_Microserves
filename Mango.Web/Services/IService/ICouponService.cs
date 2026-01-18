05569457b71cb7edecd3c089f1558d6a261e6bcc
csmohamme <cs.mohammedalmostafa@gmail.com>
2026-01-18 21:14:58 +0300

    ICoupon service

---
+++ Mango.Web/Services/IService/ICouponService.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Services.IService
+{
+    public interface ICouponService
+    {
+        Task<ResponseDto?> GetAllCouponAsync();
+        Task<ResponseDto?> GetCouponByCodeAsync(string couponCode);
+        Task<ResponseDto?> GetCouponByIdAsync(int couponId);
+        Task<ResponseDto?> CreateCouponAsync(CouponDto couponDto);
+        Task<ResponseDto?> UpdateCouponAsync(CouponDto couponDto);
+        Task<ResponseDto?> DeleteCouponAsync(int couponId);
+
+    }
+}
