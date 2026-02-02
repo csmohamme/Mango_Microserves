efdacc7a0192a45e43788f8284a3c1ef7fa8c043
csmohamme <cs.mohammedalmostafa@gmail.com>
2026-02-02 20:19:51 +0300

    Add custom property to user table

--- Mango.Services.AuthAPI/Data/AppDbContext.cs
+++ Mango.Services.AuthAPI/Data/AppDbContext.cs
@@ -1,16 +1,18 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Mango.Services.AuthAPI.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.AuthAPI.Data
 {
-    public class AppDbContext : IdentityDbContext<IdentityUser>
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
             
         }
 
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         // Seeding initial data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
