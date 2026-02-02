6b545a271bc8acc8971bf1183e07743caf698a91
csmohamme <cs.mohammedalmostafa@gmail.com>
2026-02-02 20:33:15 +0300

    Endpoints for regester and login

---
+++ Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.AuthAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+        // ================================= Register API =================================
+        [HttpPost("register")]
+        public async Task<IActionResult> Register()
+        {
+            return Ok("Register API is working!");
+        }
+
+        // ================================= Login API =================================
+        [HttpPost("login")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok("Login API is working!");
+        }
+    }
+}
